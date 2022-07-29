0cbcc83697d8c1bba51f7301d3f0e8f6e2185cf0
MC <mariclaire.de-guia@ttu.edu>
2022-07-29 03:49:09 -0500

    home, projects, and links added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { LinksComponent } from './views/links/links.component';
+import { ProjectsComponent } from './views/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'links', component: LinksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { LinksComponent } from './views/links/links.component';
+import { ProjectsComponent } from './views/projects/projects.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LinksComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
