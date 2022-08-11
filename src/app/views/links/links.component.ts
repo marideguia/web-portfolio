75989ec195d3ea0440863c88459bc1002ef0a48d
MC <mariclaire.de-guia@ttu.edu>
2022-08-11 03:39:41 -0500

    update styles

--- src/app/views/links/links.component.ts
+++ src/app/views/links/links.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-links',
   templateUrl: './links.component.html',
   styleUrls: ['./links.component.scss']
 })
 export class LinksComponent implements OnInit {
+  onResumeClick() {
+    window.open("/links/resume-2022-dg.pdf", '_blank')
+  }
+
 
   constructor() { }
 
